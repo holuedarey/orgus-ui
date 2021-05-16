9675524240ba9dc61b64263b48a8d741ff62a8f9
Michael Madume <mmadume@pajuno.com>
2021-05-16 14:36:58 +0100

    name correction

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/argus-frontend'),
+      dir: require('path').join(__dirname, './coverage/argus-front-end'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'argus-frontend'`, () => {
+  it(`should have as title 'argus-front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('argus-frontend');
+    expect(app.title).toEqual('argus-front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('argus-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('argus-front-end app is running!');
   });
 });
