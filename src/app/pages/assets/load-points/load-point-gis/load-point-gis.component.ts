864db3342ff51b3bb69037271dd82f5367dbff17
Michael Madume <mmadume@pajuno.com>
2021-09-16 13:11:44 +0100

    load point bug fix

--- src/app/pages/assets/load-points/load-point-gis/load-point-gis.component.ts
+++ src/app/pages/assets/load-points/load-point-gis/load-point-gis.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NbDateService } from '@nebular/theme';
 import { LoadPointDto } from 'src/app/@core/dtos/load-point.dto';
+import { LoadPointAnalyticsResourcesNavMap, LoadPointAnalyticsResources } from 'src/app/pages/analytics/load-point-analytics/load-point-analytics-resources';
 
 @Component({
   selector: 'app-load-point-gis',
@@ -11,7 +13,17 @@ export class LoadPointGisComponent implements OnInit {
   @Input() value!: string | number;
   @Input() rowData!: LoadPointDto;
   
-  constructor() { }
+  loadPointAnalyticsLink = LoadPointAnalyticsResourcesNavMap.get(LoadPointAnalyticsResources.ExpandedView)?.route;
+
+  
+  get monthStart(): Date {
+    return this.dateService.getMonthStart(new Date());
+  }
+  get monthEnd(): Date {
+    return this.dateService.getMonthEnd(new Date());
+  }
+
+  constructor(private dateService: NbDateService<Date>) { }
 
   ngOnInit(): void {
     console.log('')
--- src/app/pages/assets/load-points/load-points.module.ts
+++ src/app/pages/assets/load-points/load-points.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { LoadPointsRoutingModule } from './load-points-routing.module';
 import { LoadPointFormComponent } from './load-point-form/load-point-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NbDialogModule, NbCardModule, NbIconModule, NbAlertModule, NbInputModule, NbSpinnerModule, NbButtonModule, NbSelectModule, NbFormFieldModule, NbThemeModule, NbAutocompleteModule, NbToggleModule } from '@nebular/theme';
+import { NbDialogModule, NbCardModule, NbIconModule, NbAlertModule, NbInputModule, NbSpinnerModule, NbButtonModule, NbSelectModule, NbFormFieldModule, NbThemeModule, NbAutocompleteModule, NbToggleModule, NbTooltipModule } from '@nebular/theme';
 import { TablesModule } from 'src/app/@tables/tables.module';
 import { LoadPointStatusToggleComponent } from './load-point-status-toggle/load-point-status-toggle.component';
 import { LoadPointGisComponent } from './load-point-gis/load-point-gis.component';
@@ -37,6 +37,7 @@ import { LoadPointGisComponent } from './load-point-gis/load-point-gis.component
     NbAutocompleteModule,
     NbThemeModule,
     NbToggleModule,
+    NbTooltipModule
   ]
 })
 export class LoadPointsModule { }
